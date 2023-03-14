de9c4f57b6829980fc27fa2dddc820a56cb5b7ab
slavasergeev <youail>
2023-03-14 20:39:20 +0700

    program ver2

--- main_ver2.py
+++ main_ver2.py
@@ -1,4 +1,3 @@
-# importing the required modules
 import tkinter as tk  # importing the tkinter module as tk
 from tkinter import ttk  # importing the ttk module from the tkinter library
 from tkinter import messagebox  # importing the messagebox module from the tkinter library
